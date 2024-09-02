4a7b1ae9f02be32582f439b102b32ab40c9168f7
Torres-Alan <adtorresfuentes14@gmail.com>
2024-09-02 14:08:13 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# reto1_donut_app_alan_torres
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/reto1_donut_app_alan_torres/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.reto1_donut_app_alan_torres
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
