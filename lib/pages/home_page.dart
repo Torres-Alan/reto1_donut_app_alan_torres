7bf40f7351dbc361e641b46952ccd4105ca37ade
Torres-Alan <adtorresfuentes14@gmail.com>
2024-09-05 15:16:11 -0600

    Añadiendo texto principal

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,8 +13,39 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
-        leading: Icon(Icons.menu, color: Colors.grey[800]),
+        leading: Padding(
+          padding: const EdgeInsets.only(left:24.0),
+          child: IconButton(
+          icon: Icon(Icons.menu, color: Colors.grey[800], size:36), 
+          onPressed: () { 
+            print('Botón de Menú'); },),
+        ),
+        actions: [Padding(
+          padding: const EdgeInsets.only(right:24.0),
+          child: IconButton(
+            icon: Icon(Icons.person, color:Colors.grey[800], size:36), onPressed: () { 
+            print('Botón de Usuario'); },),
+        )],
       ),
+      body: const Column(children: [
+        //TEXTO "I want to eat"
+        Padding(
+          padding:EdgeInsets.all(24.0),
+          child: Row(
+            children: [
+              Text("I want to ", style: TextStyle(fontSize: 32)),
+              Text("Eat", style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                decoration: TextDecoration.underline)),
+            ],
+          ),
+        )
+        //Tap Bar
+
+
+        //Tap view
+      ]),
     );
   }
 }
